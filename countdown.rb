e3737fe6465ce33a985c54ed8defc0a8fb9f5659
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-30 02:24:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    break if number = 0 
+    return "HAPPY NEW YEAR!"
+  end
 end
+
+def countdown_with_sleep
+  sleep(5)
+end
+
+
+while x > 10
+  puts "#{x} seconds"
+  x -= 1
+end
+
+
