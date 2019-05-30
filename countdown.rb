6ca3dea152428c1c0f7fb2dbbf80de1d3c402b95
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-30 02:26:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,9 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    break if number = 0 
-    return "HAPPY NEW YEAR!"
+    if number = 0 
+      return "HAPPY NEW YEAR!"
+    end
   end
 end
 
