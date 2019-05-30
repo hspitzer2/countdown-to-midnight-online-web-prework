7ecc746bab60f87c043f4cab3442779512ed4402
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-30 02:42:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,9 +8,9 @@ def countdown(number)
   end
 end
 
-def countdown_with_sleep(nap)
+def countdown_with_sleep(number)
   while number > 0
-    sleep(nap)
+    sleep(1)
     puts "#{number} SECOND(S)!"
     number -= 1
     if number == 0 
